0cf3910a7120ee850367ec7434840fd434699971
notveryhelpfuldude <caracass@hotmail.no>
2025-10-13 21:50:09 +0200

    Adding temporary sign in to auth

--- src/state/auth.ts
+++ src/state/auth.ts
@@ -1,5 +1,14 @@
+import SignIn from "../../app/(auth)/sign-in"
+
 export function useAuth() {
-    const token = "admin"
-    const role = "admin"
-    return {token, role}
+    const token = "test"
+    const role = "test"
+    //midlertidig signin funksjon, må gjøres async med database etc...
+    const signIn = async (newToken: string, newRole: string) => {
+    try {
+    } catch (err) {
+      console.error('Error, you failed:', err);
+    }
+  };
+    return {token, role, signIn}
 }
