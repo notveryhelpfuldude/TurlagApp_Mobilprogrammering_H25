2c5d0b17cf30875de4e9c24aca2e33be8931b7d1
notveryhelpfuldude <caracass@hotmail.no>
2025-10-13 21:59:29 +0200

    oppdaterte parametre

--- src/state/auth.ts
+++ src/state/auth.ts
@@ -4,8 +4,9 @@ export function useAuth() {
     const token = "test"
     const role = "test"
     //midlertidig signin funksjon, må gjøres async med database etc...
-    const signIn = async (newToken: string, newRole: string) => {
+    const signIn = async ({token, role, displayName}) => {
     try {
+        
     } catch (err) {
       console.error('Error, you failed:', err);
     }
