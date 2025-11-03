7a371f7086686e0a03ed963fffaa3c9a8435e6dd
notveryhelpfuldude <caracass@hotmail.no>
2025-11-03 13:17:00 +0100

    Oppdaterte index til å håndtere ny auth

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,13 @@ import { Redirect } from 'expo-router';
 import { useAuth } from '../src/state/auth';
 
 export default function Index() {
-  const { token, role } = useAuth();
-  if (!token) return <Redirect href="/(auth)/sign-in" />;
+  const {context} = useAuth();
+  if (context.user == null) return <Redirect href="/(auth)/sign-in" />;
 
-  switch (role) {
-    case 'tourist': return <Redirect href="/(tourist)/" />;
-    case 'guide':   return <Redirect href="/(guide)/tours" />;
-    case 'admin':   return <Redirect href="/(admin)/dashboard" />;
+  switch (context.user.role) {
+    case "USER":    return <Redirect href="/(tourist)/" />;
+    case "GUIDE":   return <Redirect href="/(guide)/tours" />;
+    case "ADMIN":   return <Redirect href="/(admin)/dashboard" />;
     default:        return <Redirect href="/(auth)/sign-in" />;
   }
 }
