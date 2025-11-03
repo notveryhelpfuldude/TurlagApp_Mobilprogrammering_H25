81997250abd4b9a53189847093a1c171121a875f
notveryhelpfuldude <caracass@hotmail.no>
2025-11-03 15:43:26 +0100

    La til testbrukere

---
+++ src/types/usertype.ts
@@ -0,0 +1,25 @@
+interface UserType {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+}
+
+const adminUser: UserType = {
+    id: '1',
+    name: 'Admin User',
+    email: 'admintest',
+    role: 'admin',
+};
+const guideUser: UserType = {
+    id: '2',
+    name: 'Guide User',
+    email: 'guidetest',
+    role: 'guide',
+};
+const touristUser: UserType = {
+    id: '3',
+    name: 'Tourist User',
+    email: 'touristtest',
+    role: 'tourist',
+};
