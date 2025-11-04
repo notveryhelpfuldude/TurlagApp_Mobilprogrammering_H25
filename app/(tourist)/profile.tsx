20995fb7bb8eebba09d66e1df68570555080c428
notveryhelpfuldude <caracass@hotmail.no>
2025-11-04 10:14:03 +0100

    navigation for log out

--- app/(tourist)/profile.tsx
+++ app/(tourist)/profile.tsx
@@ -1,14 +1,17 @@
 import { View, Text, Button } from 'react-native';
 import { useAuth } from '../../src/state/auth';
+import { router } from 'expo-router';
 
 export default function TouristProfile() {
   async function signOut() {
     const { logout } = useAuth();
+    alert("Logging out");
     try {
       await logout();
     } catch (error) {
       console.error("Logout failed:", error);
     } finally {
+      router.replace('/(auth)/sign-in');
     }
   }
   return (
