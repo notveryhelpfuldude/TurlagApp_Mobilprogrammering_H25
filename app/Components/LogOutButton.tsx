298fa96df43488371c96dfff853e17e4dc8e75f7
notveryhelpfuldude <caracass@hotmail.no>
2025-11-23 14:57:21 +0100

    LogoutButton bruker Pressable istedenfor TOpacity

--- app/Components/LogOutButton.tsx
+++ app/Components/LogOutButton.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Text } from 'react-native';
+import { Pressable, Text } from 'react-native';
 
 type LogoutButtonProps = {
     onPress: () => void;
@@ -11,7 +11,7 @@ export function LogOutButton({ onPress }: LogoutButtonProps) {
         }
     };
     if (!onPress) {
-        return (<TouchableOpacity
+        return (<Pressable
             style={{
                 backgroundColor: '#ef4444',
                 paddingVertical: 10,
@@ -22,21 +22,23 @@ export function LogOutButton({ onPress }: LogoutButtonProps) {
             }}
         >
             <Text style={{ color: 'white', fontWeight: '600' }}>Logg ut</Text>
-        </TouchableOpacity>);
+        </Pressable>);
     }
     return (
-        <TouchableOpacity
-            onPress={onPress}
-            style={{
-                backgroundColor: '#444fefff',
-                paddingVertical: 10,
-                paddingHorizontal: 16,
-                borderRadius: 999,
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}
-        >
-            <Text style={{ color: 'white', fontWeight: '600' }}>Logg ut</Text>
-        </TouchableOpacity>
+        <view style ={{ alignItems: 'center', justifyContent: 'center' }}>
+            <Pressable
+                onPress={handlePress}
+                style={{
+                    backgroundColor: '#ef4444',
+                    paddingVertical: 10,
+                    paddingHorizontal: 16,
+                    borderRadius: 999,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Text style={{ color: 'white', fontWeight: '600' }}>Logg ut</Text>
+            </Pressable>
+        </view>
     );
 }
