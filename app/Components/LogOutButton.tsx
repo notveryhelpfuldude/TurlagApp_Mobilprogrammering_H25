84706a6af84f14acc8e42914fbfb934126661ac3
notveryhelpfuldude <caracass@hotmail.no>
2025-11-23 14:10:01 +0100

    Alternativ logout button når function mangler

--- app/Components/LogOutButton.tsx
+++ app/Components/LogOutButton.tsx
@@ -10,11 +10,25 @@ export function LogOutButton({ onPress }: LogoutButtonProps) {
             await onPress();
         }
     };
+    if (!onPress) {
+        return (<TouchableOpacity
+            style={{
+                backgroundColor: '#ef4444',
+                paddingVertical: 10,
+                paddingHorizontal: 16,
+                borderRadius: 999,
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Text style={{ color: 'white', fontWeight: '600' }}>Logg ut</Text>
+        </TouchableOpacity>);
+    }
     return (
         <TouchableOpacity
             onPress={onPress}
             style={{
-                backgroundColor: '#ef4444',
+                backgroundColor: '#444fefff',
                 paddingVertical: 10,
                 paddingHorizontal: 16,
                 borderRadius: 999,
