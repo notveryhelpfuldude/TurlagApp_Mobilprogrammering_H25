9e76bb637e9b4f96819fe24012c651c87ddcc31d
notveryhelpfuldude <caracass@hotmail.no>
2025-11-24 17:29:52 +0100

    Legger til Nye Logout Buttons i profile sider

--- app/(admin)/profile.tsx
+++ app/(admin)/profile.tsx
@@ -1,10 +1,10 @@
 import { View, Text, Button } from 'react-native';
 import { useAuth } from '../../src/state/auth';
 import { router } from 'expo-router';
+import LogOutButton from 'app/Components/LogOutButton';
 export default function AdminProfile() {
-  const { logout } = useAuth();
   return <View style={{ padding: 16, gap: 12 }}>
     <Text>Adminprofil</Text>
-    <Button title="Logg ut" onPress={logout} />
+    <LogOutButton>
   </View>;
 }
--- app/(tourist)/profile.tsx
+++ app/(tourist)/profile.tsx
@@ -1,11 +1,13 @@
 import { View, Text, Button } from 'react-native';
 import { useAuth } from '../../src/state/auth';
 import { router } from 'expo-router';
+import LogOutButton from 'app/Components/LogOutButton';
 
 export default function TouristProfile() {
   return (
     <View style={{ padding: 16, gap: 12 }}>
       <Text>Profil: { 'Ukjent'}</Text>
+      <LogOutButton/>
     </View>
   );
 }
