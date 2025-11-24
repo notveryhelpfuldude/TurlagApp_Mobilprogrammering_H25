4104c99824fbbe056a5c9e5de04474a34491e574
notveryhelpfuldude <caracass@hotmail.no>
2025-11-24 19:35:34 +0100

    Appwrite til å bruke keys

--- Appwrite/providers/index.ts
+++ Appwrite/providers/index.ts
@@ -3,6 +3,6 @@ import { APPWRITE_KEYS } from "Appwrite/constants/keys";
 
 export const ClientAppwrite = new Client();
 ClientAppwrite
-    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT || 'https://fra.cloud.appwrite.io/v1') // Your Appwrite Endpoint
-    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID || '69249441001d031c58af');
+    .setEndpoint(APPWRITE_KEYS.API_URL as string)
+    .setProject(APPWRITE_KEYS.PROJECT_ID as string);
 export const AccountAppwrite = new Account(ClientAppwrite);
