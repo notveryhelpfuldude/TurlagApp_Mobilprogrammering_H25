5e83e365ce6e6c159315d28cc1b354918d6c185b
notveryhelpfuldude <caracass@hotmail.no>
2025-11-24 17:28:36 +0100

    fjerner gamle ubrukte logout funksjoner

--- app/(admin)/profile.tsx
+++ app/(admin)/profile.tsx
@@ -2,17 +2,6 @@ import { View, Text, Button } from 'react-native';
 import { useAuth } from '../../src/state/auth';
 import { router } from 'expo-router';
 export default function AdminProfile() {
-    async function signOut() {
-      const { logout } = useAuth();
-      alert("Logging out");
-      try {
-        await logout();
-      } catch (error) {
-        console.error("Logout failed:", error);
-      } finally {
-        router.replace('/(auth)/sign-in');
-      }
-    }
   const { logout } = useAuth();
   return <View style={{ padding: 16, gap: 12 }}>
     <Text>Adminprofil</Text>
--- app/(tourist)/profile.tsx
+++ app/(tourist)/profile.tsx
@@ -3,21 +3,9 @@ import { useAuth } from '../../src/state/auth';
 import { router } from 'expo-router';
 
 export default function TouristProfile() {
-  async function signOut() {
-    const { logout } = useAuth();
-    alert("Logging out");
-    try {
-      await logout();
-    } catch (error) {
-      console.error("Logout failed:", error);
-    } finally {
-      router.replace('/(auth)/sign-in');
-    }
-  }
   return (
     <View style={{ padding: 16, gap: 12 }}>
       <Text>Profil: { 'Ukjent'}</Text>
-      <Button title="Logg ut" onPress={signOut} />
     </View>
   );
 }
