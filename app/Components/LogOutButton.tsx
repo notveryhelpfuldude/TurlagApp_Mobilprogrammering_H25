648fcb1db439e22691aedf2b1dd43e6df3a8ea2c
notveryhelpfuldude <caracass@hotmail.no>
2025-11-24 17:23:51 +0100

    riktig bruk av hook i LogoutButton

--- app/Components/LogOutButton.tsx
+++ app/Components/LogOutButton.tsx
@@ -1,22 +1,31 @@
 import { Pressable, Text } from 'react-native';
+import { useAuth } from '../../src/state/auth';
+import { router } from 'expo-router';
 
 type LogoutButtonProps = {
     onPress?: () => Promise<void> | void;
 };
 
 export default function LogOutButton({ onPress }: LogoutButtonProps) {
+    const {logout} = useAuth();
     const handlePress = async () => {
-        if (onPress) {
-            try {
-                await onPress();
-            } catch (error) {
-                console.error('Logout failed:', error);
-            }
+    if (onPress) {
+        await onPress();
+    } else {
+        alert("Logging out");
+        try {
+            await logout();
+        } catch (error) {
+            console.error("Logout failed:", error);
+        } finally {
+            router.replace('/(auth)/sign-in');
         }
+    }
     };
     if (!onPress) {
         return (
         <Pressable
+            onPress={handlePress}
             style={{
                 backgroundColor: '#ef4444',
                 paddingVertical: 10,
