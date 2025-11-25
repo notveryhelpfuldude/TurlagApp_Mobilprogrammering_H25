f9a075c51b8c71e7e7867bddb2de55bd6c3f9da6
notveryhelpfuldude <caracass@hotmail.no>
2025-11-25 15:38:56 +0100

    Appwrite index, Unngår Keys for øyeblikket

--- Appwrite/providers/index.ts
+++ Appwrite/providers/index.ts
@@ -1,10 +1,18 @@
 import { Client, TablesDB, Account } from "react-native-appwrite";
 import { APPWRITE_KEYS } from "Appwrite/constants/keys";
 
-export const client = new Client();
-client
-    .setEndpoint(APPWRITE_KEYS.API_URL)
-    .setProject(APPWRITE_KEYS.PROJECT_ID)
-    .setPlatform(APPWRITE_KEYS.PLATFORM_ID);
+const endpoint = process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT;
+const projectId = process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID;
+const platformId = process.env.EXPO_PUBLIC_APPWRITE_PLATFORM_ID;
+
+if (!endpoint || !projectId) {
+  throw new Error('Missing Appwrite env vars: EXPO_PUBLIC_APPWRITE_ENDPOINT or EXPO_PUBLIC_APPWRITE_PROJECT_ID');
+}
+
+export const client = new Client()
+  .setEndpoint(endpoint)
+  .setProject(projectId)
+  .setPlatform(platformId);
+
 export const account = new Account(client);
 export const database = new TablesDB(client);
