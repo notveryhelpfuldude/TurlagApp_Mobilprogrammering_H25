a4b5a9969004dead954b1968f82489f35efa0af3
notveryhelpfuldude <caracass@hotmail.no>
2025-11-26 19:48:27 +0100

    SignUp funksjon

--- app/Components/SignUpForm.tsx
+++ app/Components/SignUpForm.tsx
@@ -1,9 +1,24 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import Checkbox from 'expo-checkbox';
+import { router } from 'expo-router';
+import { useAuth, ROLES, type Role } from '../../Appwrite/providers/auth'; 
+
+
 
 export default function SignUpForm() {
+
+    
+
+    const HandleSignUp = () => {
+        const register = async (email: string, password: string, role: Role) => {
+
+        };
+    };
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Sign Up Form Placeholder</Text>
+
         </View>
     );
 }
