fce99cce14ed956953b3dd061bcf18fbd040564b
notveryhelpfuldude <caracass@hotmail.no>
2025-11-26 18:52:42 +0100

    Sign Up Form laget

---
+++ app/(auth)/SignUpForm.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function SignUp() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Sign Up Form Placeholder</Text>
+        </View>
+    );
+}
