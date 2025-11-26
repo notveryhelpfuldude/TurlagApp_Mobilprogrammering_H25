6fba710f75a9921cf85d369c9fcb227f25cb3773
notveryhelpfuldude <caracass@hotmail.no>
2025-11-26 19:00:47 +0100

    Fixer route, Loginform -> sign-in

--- app/index.tsx
+++ app/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Text style={{ fontSize: 22, fontWeight: "700" }}>Velkommen 🎉</Text>
 
       {/* Link som åpner LoginForm-siden */}
-      <Link href="/(auth)/LoginForm" asChild>
+      <Link href="/(auth)/sign-in" asChild>
         <Pressable
           style={{
             backgroundColor: "#2563eb",
