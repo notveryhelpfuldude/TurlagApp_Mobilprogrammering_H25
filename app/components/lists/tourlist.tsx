d17d5e288ad75c69ec15337eeeaf2c9a55e47041
notveryhelpfuldude <caracass@hotmail.no>
2025-11-27 21:35:03 +0100

    la til States i tourlist

--- app/components/lists/tourlist.tsx
+++ app/components/lists/tourlist.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { use, useState } from "react";
 import { TourCard } from "../TourCard";
 import { Tour } from "@/src/data/tours";
 
+export default function TourList() {
+    const [tours, setTours] = useState<Tour[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+}
