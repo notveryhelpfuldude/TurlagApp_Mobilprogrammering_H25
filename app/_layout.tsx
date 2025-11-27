97ee207b8d19bb94d05c51b95a3d72971727efb1
notveryhelpfuldude <caracass@hotmail.no>
2025-11-27 23:27:21 +0100

    funksjon for å redirecte bruker

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,38 @@ import AuthProvider, {useAuth} from '../Appwrite/providers/auth';
 import { Children, useEffect } from "react";
 import { account, testConnection } from "Appwrite/providers";
 import "./tailwind.css";
+import { router } from "expo-router";
 
 
-export  function RootLayout() {
+export default function RootLayout() {
+  
   return (
     <AuthProvider>
-      {Children.only(<Slot />)}
+      <AuthGateway />
     </AuthProvider>
   );
+}
+function AuthGateway() {
+  const { user, isLoading } = useAuth();
+  useEffect(() => {
+    console.log("AuthGateway - user:", user, "isLoading:", isLoading);
+    if (isLoading) return;
+    if (user) {
+      switch (user.role) {
+        case "admin":
+          router.replace("/(admin)/profile");
+          break;
+        case "guide":
+          router.replace("/(guide)/");
+          break;
+        case "tourist":
+          router.replace("/(tourist)/");
+          break;
+        default:
+          router.replace("/(auth)/sign-in");
+      }
+    }
+  }, [user, isLoading]);
+
+   return <Slot />;
 }
