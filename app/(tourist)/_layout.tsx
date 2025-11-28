ad51f5e3a23cfa247dba06817bcaddfaacab96a4
notveryhelpfuldude <caracass@hotmail.no>
2025-11-28 16:48:14 +0100

    Oppdaterer nav i tourist

--- app/(tourist)/_layout.tsx
+++ app/(tourist)/_layout.tsx
@@ -1,12 +1,28 @@
 import { Tabs, Redirect } from 'expo-router';
 import { useAuth } from '../../Appwrite/providers/auth';
+import { useColorScheme } from 'react-native';
 
 export default function TouristTabs() {
+    const theme = useColorScheme();
   
   //if (!token || role !== 'tourist') return <Redirect href="/(auth)/sign-in" />;
 
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      initialRouteName="index"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme === "dark" ? "#ffffff" : "#111827",
+        tabBarInactiveTintColor: "#9CA3AF",
+        tabBarStyle: {
+          backgroundColor: theme === "dark" ? "#0B1220" : "#ffffff",
+          borderTopColor: "#E5E7EB",
+          height: 100,
+          paddingTop: 6,
+        },
+        tabBarLabelStyle: { fontSize: 12, fontWeight: "600" },
+      }}
+    >
       <Tabs.Screen name="index"    options={{ title: 'Hjem' }} />
       <Tabs.Screen name="wishlist" options={{ title: 'Ønsker' }} />
       <Tabs.Screen name="bookings" options={{ title: 'Bestill.' }} />
