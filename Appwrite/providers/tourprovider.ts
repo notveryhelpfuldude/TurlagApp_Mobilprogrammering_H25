0c98364702627f93cf2d3303b7be3cf395a910e8
notveryhelpfuldude <caracass@hotmail.no>
2025-11-28 13:41:45 +0100

    AddTour funksjon i tourprovider

--- Appwrite/providers/tourprovider.ts
+++ Appwrite/providers/tourprovider.ts
@@ -1,4 +1,4 @@
-import { Query, TablesDB } from "react-native-appwrite";
+import { Query, TablesDB, ID } from "react-native-appwrite";
 import { APPWRITE_KEYS } from "../constants/keys";
 import { client } from "./index";
 import { Tour } from "@/src/data/tours";
@@ -19,4 +19,31 @@ export async function listAllTours() {
   });
 
   return (res as any)?.rows ?? [];
+}
+
+export async function addTour(tourData: Omit<Tour, "$id" | "$collectionId" | "$databaseId" | "$createdAt" | "$updatedAt" | "$permissions">): Promise<Tour> {
+    if (!databaseId || !tourTableId) {
+        throw new Error("Missing EXPO_PUBLIC_DATABASE_ID or EXPO_PUBLIC_TOURS_TABLE_ID");
+    }
+    const rowData = {
+        title: tourData.title,
+        location: tourData.location,
+        tourDescription: tourData.tourDescription,
+        startDate: tourData.startDate,
+        endDate: tourData.endDate,
+        priceNOK: tourData.priceNOK,
+        maxParticipants: tourData.maxParticipants,
+        distanceKM: tourData.distanceKM,
+        difficulty: tourData.difficulty,
+        spotsLeft: tourData.spotsLeft,
+        imageURL: tourData.imageURL,
+    };
+    const rowId = ID.unique();
+    const res = await database.createRow({
+        databaseId,
+        tableId: tourTableId,
+        rowId,
+        data: rowData,
+    });
+    return res as unknown as Tour;
 }
