eb260457c851940832fb8ee48deafb375e54774e
notveryhelpfuldude <caracass@hotmail.no>
2025-11-28 17:19:05 +0100

    admin side bruker AppTabs

--- app/(admin)/_layout.tsx
+++ app/(admin)/_layout.tsx
@@ -1,15 +1,16 @@
 import { Tabs, Redirect } from 'expo-router';
 import { useAuth } from '../../Appwrite/providers/auth';
+import { AppTabs } from '../components/AppTabs';
 
 export default function AdminTabs() {
  // if (!token || role !== 'admin') return <Redirect href="/(auth)/sign-in" />;
 
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <AppTabs>
       <Tabs.Screen name="dashboard" options={{ title: 'Oversikt' }} />
       <Tabs.Screen name="tours"     options={{ title: 'Turer' }} />
       <Tabs.Screen name="users"     options={{ title: 'Brukere' }} />
       <Tabs.Screen name="profile"   options={{ title: 'Profil' }} />
-    </Tabs>
+    </AppTabs>
   );
 }
