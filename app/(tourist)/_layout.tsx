14ff40c476bdb00c8a21af33bba35a6143ba6912
notveryhelpfuldude <caracass@hotmail.no>
2025-11-28 16:50:13 +0100

    Legger til ikoner til tabs i tourist

--- app/(tourist)/_layout.tsx
+++ app/(tourist)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs, Redirect } from 'expo-router';
 import { useAuth } from '../../Appwrite/providers/auth';
 import { useColorScheme } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TouristTabs() {
     const theme = useColorScheme();
@@ -23,10 +24,42 @@ export default function TouristTabs() {
         tabBarLabelStyle: { fontSize: 12, fontWeight: "600" },
       }}
     >
-      <Tabs.Screen name="index"    options={{ title: 'Hjem' }} />
-      <Tabs.Screen name="wishlist" options={{ title: 'Ønsker' }} />
-      <Tabs.Screen name="bookings" options={{ title: 'Bestill.' }} />
-      <Tabs.Screen name="profile"  options={{ title: 'Profil' }} />
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: 'Hjem',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="wishlist" 
+        options={{ 
+          title: 'Ønsker',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-outline" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="bookings" 
+        options={{ 
+          title: 'Bestill.',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar-outline" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="profile" 
+        options={{ 
+          title: 'Profil',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
+          ),
+        }} 
+      />
       <Tabs.Screen name="activity/[id]" options={{ href: null }} />
       <Tabs.Screen name="checkout/index" options={{ href: null }}/>
       <Tabs.Screen name="review/[bookingId]" options={{ href: null }}/>
